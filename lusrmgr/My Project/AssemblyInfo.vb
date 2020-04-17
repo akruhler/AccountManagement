bada41511263b4bab692572591863eb9e597eabc
proviq <63720596+proviq@users.noreply.github.com>
2020-04-17 15:20:48 +0200

    Add files via upload

---
+++ lusrmgr/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die mit einer Assembly verknüpft sind.
+
+' Die Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("Local user and group management")> 
+<Assembly: AssemblyDescription("Tool which allows you to easily manage the users and groups on local computers.")> 
+<Assembly: AssemblyCompany("A. Krühler")> 
+<Assembly: AssemblyProduct("Local user and group management")> 
+<Assembly: AssemblyCopyright("Copyright © 2017-2020 A. Krühler")> 
+<Assembly: AssemblyTrademark("A. Krühler")> 
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+<Assembly: Guid("7b7001ed-7468-42be-9ea5-f8c852bc7749")> 
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.5.0.0")> 
+<Assembly: AssemblyFileVersion("1.5.0.0")> 
